dcb18103ca5daceb094b7bdc9bfc276c6feeff85
victorsilva-ibmec <61068111+victorsilva-ibmec@users.noreply.github.com>
2020-03-06 21:12:02 -0300

    Aula do dia 06-03-20

--- ola_mundo.py
+++ ola_mundo.py
@@ -29,20 +29,6 @@ def div_por_seis(valor):
     return ((valor % 2) == 0) and ((valor % 3) == 0)
 
 
-def testes():
-    print(div_por_seis(7))
-    print(div_por_seis(9))
-    print(div_por_seis(12))
-    x = soma_dois_valores(3, 4)
-    print(x)
-    x = soma_dois_valores(2, 8)
-    print(x)
-    x = raiz(9, 2)
-    print(x)
-    print(e_par(4))
-    print(e_par(5))
-
-
 def testa_par():
     """Le um valor inserido pelo usuario, verifica se o valor e par e retorna uma mensagem."""
     parada = False
@@ -67,4 +53,54 @@ def dez_mult_tres():
         numero += 1  # numero = numero + 1
 
 
-dez_mult_tres()
+def ordena_tres_numeros(valor1, valor2, valor3):
+    if valor1 > valor2:
+        valor1, valor2 = valor2, valor1
+    if valor2 > valor3:
+        valor2, valor3 = valor3, valor2
+    if valor1 > valor2:
+        valor1, valor2 = valor2, valor1
+    print(valor1, valor2, valor3)
+
+
+def decompoe_numero(valor):
+    print(valor % 10)
+    print((valor // 10) % 10)
+    print(valor // 100)
+
+
+def e_multiplo(valor, divisor):
+    if valor % divisor == 0:
+        print('É múltiplo de', divisor)
+    else:
+        print('Não é múltiplo de', divisor)
+
+
+def informa_pares():
+    for i in range(3):
+        valor = int(input('Insira um número.\n'))
+        e_multiplo(valor, 2)
+
+
+def informa_maior():
+    maximo = 0
+    for i in range(3):
+        valor = int(input('Insira um número.\n'))
+        if valor > maximo:
+            maximo = valor
+    print('O maior valor inserido é', maximo)
+
+
+def informa_maior_alt():
+    numeros = []
+    for i in range(3):
+        numeros.append(int(input('Informe um valor.\n')))
+    print(max(numeros))
+
+
+def main():
+    informa_maior_alt()
+
+
+if __name__ == '__main__':
+    main()
