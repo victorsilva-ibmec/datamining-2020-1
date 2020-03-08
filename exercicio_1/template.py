4b7b7ce48690f815cd3613b9ba4205d5642f3959
victorsilva-ibmec <61068111+victorsilva-ibmec@users.noreply.github.com>
2020-03-08 12:15:16 -0300

    Atualização do templat

--- exercicio_1/template.py
+++ exercicio_1/template.py
@@ -1,1 +1,30 @@
-"""Template para exercicio 1."""
+"""Template para os exercicios 8, 11, 12 e 14 da Aula 02."""
+def exercicio_8(valor):
+    print('')
+
+
+def exercicio_11():
+    print('')
+
+
+def exercicio_12():
+    print('')
+
+
+def exercicio_14(valor_fah):
+    print('')
+
+
+def main():
+    exercicio_8(4.85)
+    exercicio_8(5.5)
+    exercicio_8(16)
+    exercicio_11()
+    exercicio_12()
+    exercicio_14(232)
+    exercicio_14(18.33)
+    exercicio_14(-150)
+
+
+if __name__ == '__main__':
+    main()
