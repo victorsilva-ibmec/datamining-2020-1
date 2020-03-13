11e2170411eed09bee1bf8c5b9297c338810e8a4
victorsilva-ibmec <61068111+victorsilva-ibmec@users.noreply.github.com>
2020-03-13 11:03:05 -0300

    Template para o exercício 2

---
+++ exercicio_2/template.py
@@ -0,0 +1,39 @@
+"""Template para os exercicios 3, 5, 12, 18 e 21 da Aula 04."""
+def exercicio_3(peso):
+    print('')
+
+
+def exercicio_5(area_pintura):
+    print('')
+
+
+def exercicio_12(a, b, c):
+    print('')
+
+
+def exercicio_18(termo_n):
+    print('')
+
+
+def exercicio_21(total_eleitores):
+    print('')
+
+
+def main():
+    exercicio_3(-10)
+    exercicio_3(4.25)
+    exercicio_3(62.8)
+    exercicio_5(54)
+    exercicio_5(63.9)
+    exercicio_5(25)
+    exercicio_12(1, 0, 0)
+    exercicio_12(5, 2, 0)
+    exercicio_12(2, 4, 6)
+    exercicio_18(4)
+    exercicio_18(8)
+    exercicio_21(5)
+    exercicio_21(20)
+    
+
+if __name__ == '__main__':
+    main()
