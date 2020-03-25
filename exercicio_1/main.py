efc9f7dd90a01925d35c25cd19e33161075a458f
rcrescente <61852847+rcrescente@users.noreply.github.com>
2020-03-25 10:21:26 -0300

    Add files via upload
    Professor, segue o arquivo com os exercícios da lista 1

---
+++ exercicio_1/main.py
@@ -0,0 +1,52 @@
+def ex8():
+
+valor = input('Insira um numero')
+
+        inteiro = int(valor)
+        decimal = float(valor - inteiro)
+    print('A parte inteira é igual a '+ inteiro +' e a parte decimal é igual a '+ decimal +'!')
+
+
+def ex11():
+    num_bois = int(input('Insira a quantidade de bois'))
+    pesomax = 0
+    idenmax = 0
+
+    for i in range(num_bois):
+        idboi = int(input('Insira o id do boi'))
+        pesoboi = int(input('Insira o peso do boi'))
+
+    if pesoboi > pesomax:
+            pesomax = peso_boi
+            idenmax = iden
+    print('O boi mais pesado é o boi com identificação:', iden_max, 'com peso de :', peso_max)
+
+
+def ex12():
+
+    soma = 0
+    n = int(input('insira a quantidade de numeros na analise'))
+
+    for i in range(n)
+        numero = float(input('Insira o numero '+ i))
+        soma = soma + numero
+
+    print('A média aritimetica dos numeros é: '+ soma/n)
+
+
+def exercicio_14():
+    valorf
+    valorc = 5*(valorf-32) / 9
+    print('A temperatura em celsius é',valorc)
+
+
+
+def main():
+    exercicio_8(4.85)
+    exercicio_8(5.5)
+    exercicio_8(16)
+    exercicio_11()
+    exercicio_12()
+    exercicio_14(232)
+    exercicio_14(18.33)
+    exercicio_14(-150)
